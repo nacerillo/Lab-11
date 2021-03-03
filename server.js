2ef689ba85807b37f5f24460412a6ca0039c9ac5
nacerillo <nicholascerillo@gmail.com>
2021-03-03 14:16:12 -0800

    making changes to the DB, and Add Book now working

--- server.js
+++ server.js
@@ -10,28 +10,43 @@ const pg = require('pg');
 const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 client.on('error', error => console.log(error));
-
+//app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
-
-
+//details action method for delete /books/:id?_method=DELETE
 app.get('/', renderHomePage);
 app.get('/searches/new', getNew);
 app.post('/searches', postSearch);
 app.post('/books', addSingleBook);
 app.get('/book/:id', getSingleBook);
+//app.get('/books/:id',deleteBook);
+//app.get('/books/:id',updateBook);
+//  app.post('/books')
+// getData from DB
 
-//const url = 'https://www.googleapis.com/books/v1/volumes?q=Dune';
 
+/*function updateBook(req,res){
+    /go get a book from DB, who's id = req.params.id
+    //pass book into ejs file as an object
+
+}*/
+/*function deleteBook(req,res){
+      //get id = req.params.id
+    //run delete query with that Id
+    // redirect them elsewhere
+}*/
 function addSingleBook(req, res) {
     //(author,title,isbn,image_url,description) 
-    const sqlString = 'INSERT INTO book (author, title, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)';
+    const sqlString = 'INSERT INTO book (author, title, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) returning *';
     const sqlArray = [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.description];
+    //console.log(sqlArray);
+
     client.query(sqlString, sqlArray).then(result => {
         const book = result.rows[0];
-        let ejObject = { book };
-        res.redirect('pages/details.ejs');
+        console.log(book);
+        // let ejObject = { book };
+        res.redirect(`/book/${book.id}`);
     });
     // console.log(req.body, "params");
 
@@ -39,7 +54,7 @@ function addSingleBook(req, res) {
 function getSingleBook(req, res) {
     // :id can be found at req.params.id
     //render a single task
-    console.log(req.params, "params");
+    //  console.log(req.params, "params");
     const sqlString = 'SELECT * FROM book WHERE ID = $1';
     const sqlArray = [req.params.id];
     client.query(sqlString, sqlArray).then(result => {
@@ -55,9 +70,9 @@ function renderHomePage(req, res) {
     // console.log(req.body);
     const sqlString = `SELECT * FROM book`;
     client.query(sqlString).then(result => {
-        console.log(result.rows);
+        //   console.log(result.rows);
         const ejsObject = { allBooks: result.rows };
-        console.log(ejsObject);
+        //  console.log(ejsObject);
         res.render('pages/index.ejs', ejsObject);
     });
     // res.render('pages/index.ejs', {});
@@ -87,7 +102,7 @@ function getNew(req, res) {
 function Books(bookData) {
     console.log(bookData);
     const placeHolder = `https://i.imgur.com/J5LVHEL.jpg`;
-    const fakeISBN = "N/A"
+    const fakeISBN = "N/A";
     this.isbn = (bookData.volumeInfo.industryIdentifiers !== undefined) ? bookData.volumeInfo.industryIdentifiers[0].type + " " + bookData.volumeInfo.industryIdentifiers[0].identifier : fakeISBN;
     const bookImg = (bookData.volumeInfo.imageLinks !== undefined) ? bookData.volumeInfo.imageLinks.thumbnail : placeHolder;
     this.title = bookData.volumeInfo.title;
@@ -103,3 +118,4 @@ client.connect().then(() => {
 });
 
 
+//heroku pg:push books_app DATABASE_URL --app book-app-nacerillo
